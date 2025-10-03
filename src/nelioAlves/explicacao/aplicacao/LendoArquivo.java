8a78b461e26eb8f7223def04915850f7ce07b8ba
Josivan Tarcio <josivantarcio@gmail.com>
2025-10-02 21:20:57 -0300

    try-with-resources

--- src/nelioAlves/explicacao/aplicacao/LendoArquivo.java
+++ src/nelioAlves/explicacao/aplicacao/LendoArquivo.java
@@ -6,25 +6,25 @@ import java.util.Scanner;
 public class LendoArquivo {
 
 	public static void main(String[] args) {
-		Scanner sc = null;
 
+		Scanner sc = null;
 		try {
+
 			InputStream in = LendoArquivo.class.getResourceAsStream("/in.txt");
-			
-			if(in == null) {
-				throw new IllegalArgumentException("Arquivo nao encontrado!");
+			if (in == null) {
+				throw new IllegalArgumentException("Don't found file or don't exist");
 			}
 			sc = new Scanner(in);
 			while (sc.hasNextLine()) {
 				System.out.println(sc.nextLine());
 			}
+
 		} catch (Exception e) {
-			System.out.println("ERRO: " + e.getMessage());
+			e.printStackTrace();
 		} finally {
 			if (sc != null) {
 				sc.close();
 			}
 		}
-
 	}
 }
